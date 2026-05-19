355fba43e35cdb80b2db0e454ba39b14b3d18b31
aayushman jangra <jan291741@gmail.com>
2026-05-19 12:44:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <filesystem>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 #include "Parser.h"
 #include "Execute.h"
 
-using std::cout, std::vector, std::cin, std::string;
+using std::cout, std::string, std::vector;
 
 int main()
 {
-    string instructions;
-    string CurrentPath = std::filesystem::current_path();
     bool stop = false;
+    cout << "\033[1;32m AyTerminal v1.1 \033[0m\n";
 
     while (!stop)
     {
-        cout << "\033[1;34m[" << CurrentPath << "]\033[32mSHELL$ \n >\033[0m ";
-        std::getline(cin, instructions);
+        string CurrentPath = std::filesystem::current_path();
+
+        string prompt =
+            "\033[1;34m[" +
+            CurrentPath +
+            "]\033[32m SHELL$ \n > \033[0m";
+
+        char* input = readline(prompt.c_str());
+
+        // CTRL + D
+        if (!input)
+            break;
+
+        string instructions = input;
+
+        // Add non-empty commands to history
+        if (!instructions.empty())
+            add_history(input);
+
+        free(input);
 
         vector<string> instructions_parsed = Parse(instructions);
 
         if (instructions_parsed.empty())
             continue;
 
-        else if (instructions_parsed[0] == "exit")
+        // EXIT
+        if (instructions_parsed[0] == "exit")
         {
             stop = true;
             continue;
         }
-        
+
+        // CD
         else if (instructions_parsed[0] == "cd")
         {
             if (instructions_parsed.size() < 2)
@@ -35,24 +57,24 @@ int main()
                 cout << "cd: missing argument\n";
                 continue;
             }
-           
+
             try
             {
-            std::filesystem::path newPath = instructions_parsed[1];
-            current_path(newPath);
-            CurrentPath = std::filesystem::current_path();
+                std::filesystem::current_path(instructions_parsed[1]);
             }
-            
-            catch(const std::filesystem::filesystem_error& e)
+
+            catch (const std::filesystem::filesystem_error& e)
             {
-                cout << "cd: " << e.what() << "\n";
+                cout << "cd: " << e.what() << '\n';
             }
         }
-        
+
+        // OTHER COMMANDS
         else
         {
             execute(instructions_parsed);
         }
     }
 
+    return 0;
 }
